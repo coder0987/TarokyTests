af18192a833cf4085968cc2bb0c3c7cd50c17f1a
Mach1Btc <allxcaps@gmail.com>
2024-11-14 21:36:29 -0600

    small play page change

--- MachTarokTS/src/_root/pages/NotFound.tsx
+++ MachTarokTS/src/_root/pages/NotFound.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
 
     return (
         <div className="flex flex-col items-center w-full h-full">
-            <div className="mt-20 mb-4 w-4/5 xl:w-3/4 h-full">
+            <div className="mt-20 w-4/5 xl:w-3/4 h-full">
                 <h1 className="h1-bold ">404 Page Not Found</h1>
                 <div className="flex items-center justify-center w-full h-full pb-[300px]">
                     <Card className="card">
