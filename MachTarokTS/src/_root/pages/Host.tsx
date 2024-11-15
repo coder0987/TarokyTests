e5eaaa91a973c08c1ed81e7d040168e96c5ddf52
Mach1Btc <allxcaps@gmail.com>
2024-11-14 22:54:27 -0600

    added on clicks for play page

--- MachTarokTS/src/_root/pages/Host.tsx
+++ MachTarokTS/src/_root/pages/Host.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Host = () => {
-    return (
-        <div className='w-full h-full flex items-center'>
-            <div>
 
+    const navigate = useNavigate();
+
+    const [gameType, setGameType] = useState("Taroky");
+    const [roomNumeral, setRoomNumeral] = useState<string>("II");
+    const [joinCode, setJoinCode] = useState<string>("GKPRD");
+
+    return (
+        <div className='w-full h-full flex flex-col items-center'>
+            <div className='w-full flex flex-row items-start'>
+                <Button
+                    className='back-button m-2'
+                    onClick={() => {
+                        // any other handling when host leaves game
+                        navigate("/play");
+                    }}>➤</Button>
+            </div>
+            <div className="mt-10 mb-40 w-4/5 xl:w-3/4 flex flex-col items-center">
+                <div className="text-3xl">{gameType}</div>
+                <Separator className='my-4 bg-gray' />
+                <div className="text-xl flex flex-row justify-between w-full"><span>Room {roomNumeral}</span> <span>Code: {joinCode}</span></div>
             </div>
         </div>
     )
--- MachTarokTS/src/components/shared/MobileTopbar.tsx
+++ MachTarokTS/src/components/shared/MobileTopbar.tsx
@@ -3,8 +3,14 @@ import { Link } from 'react-router-dom';
 const MobileTopbar = () => {
     return (
         <section className='mobile-topbar'>
-            <div className='flex gap-3 py-4 px-3'>
+            <div className='flex flex-row justify-center items-center gap-3 py-4 px-3'>
                 <Link to='/' className='flex gap-1 items-center'>
+                    <img
+                        src='/assets/logo/logo-white.png'
+                        alt='logo'
+                        width={36}
+                        height={36}
+                    />
                     <img
                         src='/assets/logo/logo-full-white.png'
                         alt='logo'
