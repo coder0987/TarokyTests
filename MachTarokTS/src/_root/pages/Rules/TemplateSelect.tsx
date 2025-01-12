50385791fdb3f8687c7d1b3c91acdc6f06bb8718
Mach1Btc <allxcaps@gmail.com>
2025-01-11 18:14:46 -0600

    tweak template layout

--- MachTarokTS/src/_root/pages/Rules/TemplateSelect.tsx
+++ MachTarokTS/src/_root/pages/Rules/TemplateSelect.tsx
@@ -2,63 +2,63 @@ import React from 'react';
 import TemplateCard from './TemplateCard';
 
 interface TemplateSelectProps {
-  templates: string[];
-  saves: string[];
-  handleTemplateSelect: (templateName : string) => void;
-  handleCustomTemplateSelect: (templateName : string) => void;
+    templates: string[];
+    saves: string[];
+    handleTemplateSelect: (templateName: string) => void;
+    handleCustomTemplateSelect: (templateName: string) => void;
 }
 
 const TemplateSelect: React.FC<TemplateSelectProps> = ({ templates, saves, handleTemplateSelect, handleCustomTemplateSelect }) => {
 
-    const handleTemplateClick = (key : number) => {
+    const handleTemplateClick = (key: number) => {
         handleTemplateSelect(templates[key]);
     }
 
     return (
-    <div className="w-full h-full flex flex-col items-center">
-      <div className="w-full p-4 flex flex-col items-start">
-        <h2>MachTarok Templates</h2>
-        <div className="flex flex-col md:flex-row items-start gap-4">
-            {templates ? (templates.map((value, key) => {
-                return (
-                    <div key={key} className="cursor-pointer" onClick={() => handleTemplateClick(key)}>
-                        <TemplateCard name={value} />
-                    </div>
-                );
-            })) : (
-                <div>
-                    <p>Loading</p>
-                </div>
-            )}
-            <div className="cursor-pointer" onClick={() => handleTemplateClick(-1)}>
-                <TemplateCard name="Continue" />
-            </div>
-            <div className="cursor-pointer" onClick={() => handleTemplateClick(-2)}>
-                <TemplateCard name="Blank" />
-            </div>
-        </div>
-      </div>
-      {saves && saves.length > 0 && 
-        (
-            <div className="w-full p-4">
-                <h2>Custom Templates</h2>
-                {
-                    templates.map((value, key) => {
+        <div className="w-full h-full flex flex-col items-center">
+            <div className="w-full p-4 flex flex-col items-start">
+                <h2 className='h2 mx-auto mb-6'>MachTarok Templates</h2>
+                <div className="flex flex-col md:flex-row items-center justify-center gap-4 mx-auto">
+                    {templates ? (templates.map((value, key) => {
                         return (
-                            <div key={key} className="cursor-pointer" onClick={() => handleCustomTemplateSelect(saves[key])}>
+                            <div key={key} className="cursor-pointer" onClick={() => handleTemplateClick(key)}>
                                 <TemplateCard name={value} />
                             </div>
                         );
-                    })
-                }
+                    })) : (
+                        <div>
+                            <p>Loading</p>
+                        </div>
+                    )}
+                    <div className="cursor-pointer" onClick={() => handleTemplateClick(-1)}>
+                        <TemplateCard name="Continue" />
+                    </div>
+                    <div className="cursor-pointer" onClick={() => handleTemplateClick(-2)}>
+                        <TemplateCard name="Blank" />
+                    </div>
+                </div>
             </div>
-        )}
-        <div className="w-full p-4">
-            <h2>Import</h2>
-            <p>Input box (coming soon)</p>
+            {saves && saves.length > 0 &&
+                (
+                    <div className="w-full p-4">
+                        <h2>Custom Templates</h2>
+                        {
+                            templates.map((value, key) => {
+                                return (
+                                    <div key={key} className="cursor-pointer" onClick={() => handleCustomTemplateSelect(saves[key])}>
+                                        <TemplateCard name={value} />
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                )}
+            <div className="w-full p-4">
+                <h2>Import</h2>
+                <p>Input box (coming soon)</p>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default TemplateSelect;
