1e554335b2df8350452df72babf6b164aecb488b
Coder0987 <ijopkids@gmail.com>
2025-12-19 21:20:00 -0600

    Add romanize function for room names (:

--- MachTarokTS/src/utils/index.ts
+++ MachTarokTS/src/utils/index.ts
@@ -17,3 +17,48 @@ export function playerPerspective(originalPlace, viewpoint) {
   //Ex. if player 0 is povinnost and player 1 is AI, then from AI's view player 3 is povinnost
   return (+originalPlace - +viewpoint + 4) % 4;
 }
+
+export function romanize(num: number): string {
+  //Code copied from https://stackoverflow.com/questions/9083037/convert-a-number-into-a-roman-numeral-in-javascript
+  if (isNaN(num)) return '' + num;
+  if (num == 4) {
+    return "IIII";
+  } //Taroky cards have IIII instead of IV, but not for the XIV
+  var digits = String(+num).split(""),
+    key = [
+      "",
+      "C",
+      "CC",
+      "CCC",
+      "CD",
+      "D",
+      "DC",
+      "DCC",
+      "DCCC",
+      "CM",
+      "",
+      "X",
+      "XX",
+      "XXX",
+      "XL",
+      "L",
+      "LX",
+      "LXX",
+      "LXXX",
+      "XC",
+      "",
+      "I",
+      "II",
+      "III",
+      "IV",
+      "V",
+      "VI",
+      "VII",
+      "VIII",
+      "IX",
+    ],
+    roman = "",
+    i = 3;
+  while (i--) roman = (key[+digits.pop() + i * 10] || "") + roman;
+  return Array(+digits.join("") + 1).join("M") + roman;
+}
